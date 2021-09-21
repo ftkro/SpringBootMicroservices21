b1b36fc847fe44f1188cc20afff5bd36d383c4d8
Takuro Fukuda <2518370+ftkro@users.noreply.github.com>
2021-09-21 14:40:30 -0400

    sync test

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,23 +6,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handle the /start endpoint
- * @return
  *
  */
 @Controller
 public class PlantPlacesController {
-	@RequestMapping(value="/start", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET)
 	public String read() {
 		return "start";
 	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, headers = {"content-type=text/json"})
+	public String readJson() {
+		return "start";
+	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+
 	@PostMapping("/start")
 	public String create() {
 		return "start";
 	}
+
 	/**
 	 * Handle the / endpoint
-	 * @return
-	 *
 	 */
 	@RequestMapping("/")
 	public String index() {
